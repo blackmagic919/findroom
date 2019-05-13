f9f069fce0a0815ebf8ed9fd773ddd9a5db0a249
Jonathan Liu <Blackmagic919@gallens-Air.fios-router.home>
2019-05-12 20:52:25 -0400

    first draft

---
+++ findroom.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('#bmmapimg').mapster({
+        fillColor: 'ff0000',
+        fillOpacity: 0.3,
+        mapKey: 'data-key'
+    });
+    $('#houseNum').change(function(){
+        hnum = $('#houseNum').val()
+        getMap($('#houseNum').val())
+    })
+});
+
+function getMap(key){
+    $('#bmmapimg')
+        .mapster('set', false, 'H1, H2, H3, H4, H5, H6, H7, H8, H9, H10, H11, H12, H13, H14, H15, H16, H17, H18, H19, H20, H21, H22, H23, H24, H25, H26, H27, H28, H29, H30, H31, H32, H33, H34, H35, H36') 
+        .mapster('set', true, key) 
+}
+
+    
---
+++ research.md
@@ -0,0 +1,4 @@
+https://www.w3schools.com/tags/tag_map.asp
+https://html.com/images/how-to-make-an-image-map/
+https://www.image-map.net/
+http://www.outsharked.com/imagemapster/
